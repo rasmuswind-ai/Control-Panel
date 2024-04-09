05a8a488a77e95bbc0084af8c542e14d1d47b819
Rasmus Wind <rasmuswind7906@gmail.com>
2024-04-09 10:26:20 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-temp
+$(document).ready(function() {
+    $('#nav-toggle').change(function() {
+        if ($(this).is(':checked')) {
+            $('.home-container.container').css('margin-left', '30%');
+        } else {
+            $('.home-container.container').css('margin-left', '15%');
+        }
+    });
+});
